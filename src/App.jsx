165d0cac908384114d5ca2a96f5c63046e568da4
SowvikSuvo <skarmoker44@gmail.com>
2025-10-05 14:09:29 +0600

    State Lifting and Add to Cooking List

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Suspense } from "react";
 import "./App.css";
 import Heading from "./Components/Heading";
 import Navbar from "./Components/Navbar";
+import OrderContainer from "./Components/OrderContainer";
+import States from "./Components/States";
+const loadOrders = () => fetch("/orders.json").then((res) => res.json());
 
 function App() {
+  const ordersPromise = loadOrders();
+
   return (
     <div>
       <header className="w-11/12 mx-auto py-3">
@@ -10,6 +16,11 @@ function App() {
       </header>
       <section>
         <Heading>Kitchen Room</Heading>
+      </section>
+      <section>
+        <Suspense fallback={<span>Loading...⏳</span>}>
+          <OrderContainer Promise={ordersPromise}></OrderContainer>
+        </Suspense>
       </section>
     </div>
   );
